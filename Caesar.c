365ffc335f591de57592c7d56c3b959d7125f6e1
Matheus S <121572273+teichmullerhodge@users.noreply.github.com>
2023-01-14 20:14:58 -0300

    Update Caesar.c

--- Caesar.c
+++ Caesar.c
@@ -30,7 +30,7 @@ char UpperCase(char Letters){
 void Usage(void){
 
 	puts("Usage: <mode> <message> <integer between 0-26>");
-    puts("modes: -e for encrypt\n       -d for decrypt");
+        puts("modes: -e for encrypt\n       -d for decrypt");
 	exit(0);
 	}
 
@@ -39,10 +39,10 @@ int main(int argc, char* argv[]){
 
 
 	if(argc!=4 || atoi(argv[2]) > 26) Usage();
-    len = strlen(argv[1]);
+        len = strlen(argv[1]);
 	if(strcmp(argv[3], "-e") == 0) ENCRYPT(argv[1], len, atoi(argv[2]));
 	if(strcmp(argv[3], "-d") == 0) DECRYPT(argv[1], len, atoi(argv[2]));
-    Usage();
+        Usage();
 	return 0;
 }
 
