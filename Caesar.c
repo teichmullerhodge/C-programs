516b1ff6debda4be071363cf69594325647c2224
Matheus S <121572273+teichmullerhodge@users.noreply.github.com>
2023-01-15 17:43:12 -0300

    Added bruteforce mode.

--- Caesar.c
+++ Caesar.c
@@ -8,30 +8,14 @@ size_t len;
 //Functions Declarations
 int DECRYPT(char *message, size_t msglen, int cyphervalue);
 int ENCRYPT(char *message, size_t msglen, int cyphervalue);
-
-char LowerCase(char Letters){
-
-        Loop = CharValue - 122;
-        Loop += 96;
-        printf("%c", Loop);
-        CharValue = '\0';
-
-        }
-
-char UpperCase(char Letters){
-
-    Loop = CharValue - 90;
-    Loop += 64;
-    printf("%c", Loop);
-    CharValue = '\0';
-}
-
+//--------------------//
 
 void Usage(void){
 
 	puts("Usage: <mode> <message> <integer between 0-26>");
-        puts("modes: -e for encrypt\n       -d for decrypt");
+        puts("modes: -e for encrypt\n       -d for decrypt\n       -b for bruteforce");
 	exit(0);
+
 	}
 
 
@@ -39,9 +23,21 @@ int main(int argc, char* argv[]){
 
 
 	if(argc!=4 || atoi(argv[2]) > 26) Usage();
+
         len = strlen(argv[1]);
-	if(strcmp(argv[3], "-e") == 0) ENCRYPT(argv[1], len, atoi(argv[2]));
-	if(strcmp(argv[3], "-d") == 0) DECRYPT(argv[1], len, atoi(argv[2]));
+
+	if(strcmp(argv[3], "-e") == 0){ ENCRYPT(argv[1], len, atoi(argv[2])); puts(""); exit(0); }
+	if(strcmp(argv[3], "-d") == 0){ DECRYPT(argv[1], len, atoi(argv[2])); puts(""); exit(0); }
+        if(strcmp(argv[3], "-b") == 0){
+                                    int initial_value = atoi(argv[2]);
+                                    while(initial_value <= 26){
+                                    DECRYPT(argv[1], len, initial_value);
+                                    puts("");
+                                    initial_value++;                                    
+                                            }
+                                    exit(0);
+                    }
+
         Usage();
 	return 0;
 }
@@ -49,33 +45,80 @@ int main(int argc, char* argv[]){
 int ENCRYPT(char *message, size_t msglen, int cyphervalue){
 
     for(int i = 0; i < msglen; i++){
-    CharValue = message[i];
-    CharValue += cyphervalue;
-    if(CharValue > 122) LowerCase(CharValue);
-    if(CharValue > 90 && CharValue - cyphervalue < 97) UpperCase(CharValue);
-    printf("%c", CharValue);
-}
-    puts("");
-    exit(0);
+    if(cyphervalue == 26 || 0){ printf("%s\n", message); exit(0); }
+    
+    ENCRYPT_CHAR:
 
+    CharValue = message[i]; 
+    CharValue += cyphervalue;
+
+    if(CharValue > 122){  
+    CharValue -= 26;
+    printf("%c", CharValue);
+    if(i < msglen){
+                    i++;
+                    goto ENCRYPT_CHAR;
+                    }
+    else{
+             return 1;
+    
+                }
+                        }
+    if(CharValue > 90 && message[i] < 97){ 
+    CharValue -= 26;
+    printf("%c", CharValue);
+    if(i < msglen){
+                    i++;
+                    goto ENCRYPT_CHAR;
+                    }
+    else{
+             return 1;
+    
+                }
     }
+    
+    printf("%c", CharValue);
+    
+    } puts("");
+      exit(0);
+            
+            }
 
 int DECRYPT(char *message, size_t msglen, int cyphervalue){
 
 
     for(int i = 0; i < msglen; i++){
-    CharValue = message[i];
+    if(cyphervalue == 26 || 0){ printf("%s\n", message); exit(0); }
+    
+    DECRYPT_CHAR:
+
+    CharValue = message[i]; 
     CharValue -= cyphervalue;
-    if(CharValue > 122) LowerCase(CharValue);
-    if(CharValue > 90 && CharValue - cyphervalue < 97) UpperCase(CharValue);
+ 
+    if(CharValue < 65){  
+        CharValue += 26;
+        printf("%c", CharValue);
+        if(i < msglen){
+                    i++;
+                    goto DECRYPT_CHAR;
+                                        }
+        else { 
+                return 1;
+                       }
+                            }
+    if(CharValue < 97 && message[i] > 90){ 
+        CharValue += 26;
+        printf("%c", CharValue);
+        if(i < msglen){
+                    i++;
+                    goto DECRYPT_CHAR;
+                                        }
+       else { 
+                 return 1;
+                       }
+                            }
+    
     printf("%c", CharValue);
-}
-    puts("");
-    exit(0);
+    }   
 
-    }
-
-
-
-
-
+            }
